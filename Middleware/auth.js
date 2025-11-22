e76d8e99ef7de19afa1d966cc5330856b66fd9ec
Chetan165 <chetan.sharma162004@gmail.com>
2025-11-22 22:51:26 +0530

    removed log statements

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -4,11 +4,9 @@ const authMiddleware = async (req, res, next) => {
   const token = req.headers.authorization.replace("Bearer ", "");
   try {
     const validToken = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(validToken);
     req.userid = validToken.UserName;
     req._id = validToken._id;
     req.Picture = validToken.Picture;
-    console.log(req.userid, req._id, req.Picture);
     next();
   } catch (err) {
     res.json({ ok: false, msg: "Invalid Token" });
